eeadb2d01f7d78c7cba290409adfe8719ac1152b
SanderEngelsman <sanderplaysde@gmail.com>
2025-03-10 11:42:47 +0100

    Begin UI

---
+++ Opdracht Per 3/Assets/Scenes/Sander/Scripts/CharacterController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Vector3 moveDir;
+    public Vector3 playerRotate;
+    public Vector3 camRotate;
+    public float speed;
+    public float sensitivity;
+    public Transform cam;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        moveDir.x = Input.GetAxis("Horizontal");
+
+        moveDir.z = Input.GetAxis("Vertical");
+
+        transform.Translate(moveDir * speed * Time.deltaTime);
+
+        playerRotate.y = Input.GetAxis("Mouse X");
+
+        camRotate.x = Input.GetAxis("Mouse Y");
+
+        transform.Rotate(playerRotate * sensitivity * Time.deltaTime);
+
+        cam.Rotate(-camRotate * sensitivity * Time.deltaTime);
+    }
+}
