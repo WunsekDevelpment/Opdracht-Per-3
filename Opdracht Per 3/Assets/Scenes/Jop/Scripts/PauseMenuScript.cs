e1578670d0999a184983f62906eed194e6998e1c
JF2923 <amerongenvanjop@outlook.com>
2025-04-02 15:34:39 +0200

    added sprites and font
    added sprites and font
    and made it so you cant open the pause menu when in the main menu

--- Opdracht Per 3/Assets/Scenes/Jop/Scripts/PauseMenuScript.cs
+++ Opdracht Per 3/Assets/Scenes/Jop/Scripts/PauseMenuScript.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 public class PauseMenuScript : MonoBehaviour
 {
     public Canvas pauseCanvas;
+    public Canvas mainMenu;
     public GameObject player;
+    public bool inMainMenu;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +20,11 @@ public class PauseMenuScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (mainMenu.GetComponent<Canvas>())
+        {
+            inMainMenu = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && !inMainMenu)
         {
             pauseCanvas.GetComponent<Canvas>().enabled = !pauseCanvas.GetComponent<Canvas>().enabled;
             player.GetComponent<PlayerMovement>().enabled = !player.GetComponent<PlayerMovement>().enabled;
